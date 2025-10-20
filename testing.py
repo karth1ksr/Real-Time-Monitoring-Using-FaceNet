7781d6ebb241345c02990fe4aa9e95e0eb1f4f09
Karthikeyan SR <97963502+karth1ksr@users.noreply.github.com>
2025-10-20 20:04:44 +0530

    Added comments to the code

--- testing.py
+++ testing.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 import numpy as np
 from numpy import load
 
+#Loading the data
 data = load('trained_faces_enc.npz')
 X_train, y_train = data['arr_0'], data['arr_1']
 
@@ -13,13 +14,16 @@ X_train, y_train = data['arr_0'], data['arr_1']
 data1 = load('test_faces_enc.npz')
 encodings, labels = data1['arr_0'], data1['arr_1']
 
+#Normalizing the data
 in_encoder = Normalizer(norm='l2')
 in_encoder.fit(X_train)
 trainX = in_encoder.transform(X_train)
 
+#Creating pipeline to predict
 svm = make_pipeline(MinMaxScaler(), SVC(kernel='rbf', C=1, gamma=0.01, probability=True))
 svm.fit(X_train, y_train)
 
+#Prediction with results printed
 op = svm.predict(encodings)
 print("Accuracy:", accuracy_score(labels, op) * 100)
 print("Recall:", recall_score(labels, op, average='weighted') * 100)
@@ -27,3 +31,4 @@ print("Precision:", precision_score(labels, op,  average='weighted') * 100)
 print("F1 Score:", f1_score(labels, op, average='weighted') * 100)
 print("Confusion Matrix:")
 print(confusion_matrix(labels, op))
+
